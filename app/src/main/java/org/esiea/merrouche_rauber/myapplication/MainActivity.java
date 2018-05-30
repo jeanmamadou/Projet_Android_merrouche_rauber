23a9602736779e2990e942909b9f1f899ca0cd08
Mathieu <rauber@et.esiea.fr>
2018-05-31 00:33:06 +0200

    boum !

--- app/src/main/java/org/esiea/merrouche_rauber/myapplication/MainActivity.java
+++ app/src/main/java/org/esiea/merrouche_rauber/myapplication/MainActivity.java
@@ -5,6 +5,19 @@ import android.content.DialogInterface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.app.DatePickerDialog;
+import android.app.NotificationManager;
+import android.content.Intent;
+import android.net.Uri;
+import android.support.v4.app.NotificationCompat;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
@@ -14,6 +27,31 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         GetCharactersServices.startActionget_all_characters(this);
+        characters =findViewById(R.id.characters_button);
+        most_popular=findViewById(R.id.most_popular_button);
+        characters.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openSecondActivity();
+            }
+        });
+        most_popular.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openThirdActivity();
+            }
+        });
     }
+
+    public void openSecondActivity(){
+        Intent intent = new Intent(MainActivity.this, SecondActivity.class);
+        startActivity(intent);
+    }
+
+    public void openThirdActivity(){
+        Intent intent = new Intent(MainActivity.this, ThirdActivity.class);
+        startActivity(intent);
+    }
+
 
 }
